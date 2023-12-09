c8819a27e351f4ead6c5f284e1dafe002070b665
Peder <116122618+pedergoodman@users.noreply.github.com>
2023-12-08 20:17:52 -0600

    add Game & Round Cont

---
+++ 2023/javascript/day_02/day_02_functions.js
@@ -0,0 +1,85 @@
+// Game Data Constructor
+class Game {
+  constructor(game, rounds) {
+    this.gameNumber = game;
+    this.rounds = rounds;
+  }
+  gameNumber;
+  rounds;
+
+  highestRed() {
+    let highest = 0;
+
+    for (const round of this.rounds) {
+      let roundRed = round.colors.red;
+
+      if (roundRed > highest) {
+        highest = roundRed;
+      }
+    }
+
+    return highest;
+  }
+
+  highestGreen() {
+    let highest = 0;
+
+    for (const round of this.rounds) {
+      let roundGreen = round.colors.green;
+
+      if (roundGreen > highest) {
+        highest = roundGreen;
+      }
+    }
+
+    return highest;
+  }
+
+  highestBlue() {
+    let highest = 0;
+
+    for (const round of this.rounds) {
+      let roundBlue = round.colors.blue;
+
+      if (roundBlue > highest) {
+        highest = roundBlue;
+      }
+    }
+
+    return highest;
+  }
+}
+// Game Round Data Constructor
+class Round {
+  constructor(roundNumber) {
+    this.roundNumber = roundNumber;
+  }
+
+  roundNumber;
+  colors = {
+    red: 0,
+    green: 0,
+    blue: 0
+  };
+
+  setRed(redValue) {
+    this.colors.red = redValue;
+  }
+
+  setGreen(greenValue) {
+    this.colors.green = greenValue;
+  }
+
+  setBlue(blueValue) {
+    this.colors.blue = blueValue;
+  }
+}
+
+
+function createGameObject() {
+  
+}
+
+
+
+export { Game, Round, };
