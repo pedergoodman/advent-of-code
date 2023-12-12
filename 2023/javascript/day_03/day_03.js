146fc83e7c5524b4bf0e3b2dae55136e245a4621
Peder <116122618+pedergoodman@users.noreply.github.com>
2023-12-11 21:51:56 -0600

    create array of numbers plus their index and length

--- 2023/javascript/day_03/day_03.js
+++ 2023/javascript/day_03/day_03.js
@@ -8,6 +8,38 @@ const markdown = fs.readFileSync(
 
 const rawEngineData = markdown.split("\n");
 
-console.log(rawEngineData[0]);
+console.log(rawEngineData[4]);
 
-rawEngineData[0]
+let test = rawEngineData[4].match(/\d+/g)
+
+
+let array = []
+let re = /\d+/g
+let match 
+
+while ((match = re.exec(rawEngineData[4])) != null) {
+  
+  console.log(`match is: `, match);
+  console.log(`Found ${match[0]} at index ${match.index}`);
+
+const buildObject = {
+  value: match[0],
+  index: match.index,
+  length: match[0].length
+}
+
+// console.log(buildObject);
+
+  array.push(buildObject)
+}
+
+console.log(array);
+
+console.log(test);
+
+
+
+// find index and length of symbols
+// find index and length of numbers
+
+// compare number index + length to symbol index  
