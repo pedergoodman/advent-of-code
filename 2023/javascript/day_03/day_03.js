5e0e5b4b8c9450ab538f9869143c513648fe9ea4
Peder <116122618+pedergoodman@users.noreply.github.com>
2023-12-13 21:56:08 -0600

    filtered to numbers touching any '*', needs touching only 2 conditional statement

--- 2023/javascript/day_03/day_03.js
+++ 2023/javascript/day_03/day_03.js
@@ -28,96 +28,154 @@ for (let i = 0; i < rawEngineData.length; i++) {
   filteredEngineData.push(row);
 }
 
-let testGroup = 2;
-// for (let i = 0; i < testGroup; i++) {
-//   // print map for each set of values
-//   for (let i = 0; i < testGroup; i++) {
-//     console.log(rawEngineData[i]);
-//   }
+// * Log results from part 1 and part 2 functions
+// log the part number total
+// console.log("Part Number total is: ", findPartNumbers());
 
-//   console.log(filteredEngineData[i]);
-// }
 
-for (let i = 0; i < testGroup; i++) {
-  console.log(rawEngineData[i]);
+for (let index = 0; index < 6; index++) {
+  console.log(rawEngineData[index]);
 }
 
-// find index and length of symbols
-// find index and length of numbers
+// log the gear ratio total
+console.log("Gear Ratio total is: ", findGearRatios());
 
-// compare number index + length to symbol index
-
-// if sybmol is
-
-// if (symbol <= startingIndex - 1 || symbol >= endingIndex + 1) {
-// }
-
-// hold the added values
-let total = 0;
-
-// for each ROW
-for (let i = 0; i < filteredEngineData.length; i++) {
-  let currentRow = filteredEngineData[i];
-  let currentRowSymbols = filteredEngineData[i].symbols;
-  let previousRowSymbols = [];
-  let nexRowSymbols = [];
-
-  if (i - 1 >= 0) {
-    previousRowSymbols = filteredEngineData[i - 1].symbols;
-  }
-
-  if (i + 1 < filteredEngineData.length) {
-    nexRowSymbols = filteredEngineData[i + 1].symbols;
-  }
-
-  // console.log('currentRowSymbols is: ', currentRowSymbols);
-  // console.log('previousRowSymbols is: ', previousRowSymbols);
-  // console.log('nexRowSymbols is: ', nexRowSymbols);
-
-  let symbolsToCheck = [
-    ...previousRowSymbols,
-    ...currentRowSymbols,
-    ...nexRowSymbols,
-  ];
-  console.log(symbolsToCheck);
-
-  // map through the values
-  console.log(`ROW: ${i+1}`);
-  
-  for (const number of currentRow.values) {
-
-    let value = number.value
-    let startIndex = number.index - 1
-    let endingIndex = number.index + number.length
+for (let index = 0; index < 6; index++) {
+  console.log(rawEngineData[index]);
+}
 
 
-    console.log(value);
-    // console.log(startIndex);
-    // console.log(endingIndex);
+// PART 1
 
-    for (const symbol of symbolsToCheck) {
-      // console.log(symbol);
-      let symbolIndex = symbol.index;
+function findPartNumbers() {
+  // hold the added values
+  let partNumberTotal = 0;
 
-      
+  // for each ROW
+  for (let i = 0; i < filteredEngineData.length; i++) {
+    let currentRow = filteredEngineData[i];
+    let currentRowSymbols = filteredEngineData[i].symbols;
+    let previousRowSymbols = [];
+    let nexRowSymbols = [];
 
-      if (symbolIndex >= startIndex && symbolIndex <= endingIndex){
-        console.log(`they're touching!!! Value is:`, number.value);
-        
-        total += Number(number.value)
-      }      
-
+    // previous row stays an empty array if index is less than 0
+    if (i - 1 >= 0) {
+      previousRowSymbols = filteredEngineData[i - 1].symbols;
     }
 
+    // previous row stays an empty array if index is more than the length of the array
+    if (i + 1 < filteredEngineData.length) {
+      nexRowSymbols = filteredEngineData[i + 1].symbols;
+    }
 
+    // Checking that captured rows are passed correctly
+    // console.log('previousRowSymbols is: ', previousRowSymbols);
+    // console.log('currentRowSymbols is: ', currentRowSymbols);
+    // console.log('nexRowSymbols is: ', nexRowSymbols);
+
+    // merge all symbols to compare with numbers
+    let symbolsToCheck = [
+      ...previousRowSymbols,
+      ...currentRowSymbols,
+      ...nexRowSymbols,
+    ];
+
+    // loop through the values
+    for (const number of currentRow.values) {
+      let numberValue = number.value;
+      let startIndex = number.index - 1;
+      let endingIndex = number.index + number.length;
+
+      // console.log(value);
+      // console.log(startIndex);
+      // console.log(endingIndex);
+
+      for (const symbol of symbolsToCheck) {
+        // console.log(symbol);
+        let symbolIndex = symbol.index;
+
+        if (symbolIndex >= startIndex && symbolIndex <= endingIndex) {
+          // console.log(`they're touching!!! Value is:`, numberValue);
+
+          partNumberTotal += Number(numberValue);
+        }
+      }
+    }
   }
 
-  // if a has a symbol touching it from either side
-  // add it to the total
-
-  // only add current rows values
-  // but check the symbol locations of the line above and below
+  return partNumberTotal;
 }
 
+// PART 2
 
-console.log(total);
+function findGearRatios() {
+  // hold the added values
+  let gearRatioTotal = 0;
+
+  // for each ROW
+  // filteredEngineData.length
+  for (let i = 0; i < 3; i++) {
+    let currentRow = filteredEngineData[i];
+    let currentRowNumbers = filteredEngineData[i].values;
+    let previousRowNumbers = [];
+    let nexRowNumbers = [];
+    
+    // previous row stays an empty array if index is less than 0
+    if (i - 1 >= 0) {
+      previousRowNumbers = filteredEngineData[i - 1].values;
+    }
+
+    // previous row stays an empty array if index is more than the length of the array
+    if (i + 1 < filteredEngineData.length) {
+      nexRowNumbers = filteredEngineData[i + 1].values;
+    }
+
+    // Checking that captured rows are passed correctly
+    // console.log('previousRowNumbers is: ', previousRowNumbers);
+    // console.log('currentRowNumbers is: ', currentRowNumbers);
+    // console.log('nexRowNumbers is: ', nexRowNumbers);
+    
+    // merge all symbols to compare with numbers
+    let numbersToCheck = [
+      ...previousRowNumbers,
+      ...currentRowNumbers,
+      ...nexRowNumbers,
+    ];
+    // console.log(numbersToCheck);
+
+const symbolsToCheck = currentRow.symbols.filter(symbol => symbol.value === '*')
+// console.log('checking FILTERED row symbols @ lvl-1: ', symbolsToCheck);
+
+
+    
+    // loop through the symbol values and see what numbers are touching a '*' symbol
+    console.log(`Row ${i+1}`);
+    for (const symbol of symbolsToCheck) {
+      // console.log('symbol in symbolsToCheck: ', symbol);
+      let symbolIndex = symbol.index
+      
+      // checking if a number touches a symbol
+      for (const number of numbersToCheck) {
+        // console.log(symbol);
+        let numberValue = number.value;
+        let startIndex = number.index - 1;
+        let endingIndex = number.index + number.length;
+        
+        
+        if (symbolIndex >= startIndex && symbolIndex <= endingIndex) {
+          console.log(`they're touching!!! SYMBOL index is:`, symbolIndex);
+          console.log(`NUMBER index range is ${startIndex}-${endingIndex}`);
+          console.log(`they're touching!!! Value is:`, numberValue);
+
+
+          //
+          gearRatioTotal += Number(numberValue);
+        }
+      } 
+
+    }
+    
+  }
+
+  return gearRatioTotal;
+}
