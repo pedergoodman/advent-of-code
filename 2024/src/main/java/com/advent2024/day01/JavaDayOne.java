2a742e79c4b13a8b2747d18275957183207416d5
Peder Goodman <peder.goodman@gmail.com>
2024-12-18 11:00:29 -0600

    add puzzle input and succefully parse it

--- 2024/src/main/java/com/advent2024/day01/JavaDayOne.java
+++ 2024/src/main/java/com/advent2024/day01/JavaDayOne.java
@@ -1,13 +1,24 @@
 package src.main.java.com.advent2024.day01;
 
+import java.util.List;
 import src.main.java.com.advent2024.utils.Day;
+import src.main.java.com.advent2024.utils.ParseInput;
 
 public class JavaDayOne implements Day {
 
     @Override
     public void runPuzzleOne() {
         System.out.println("Running Day One, Puzzle One");
+        // Read the input file and parse data
+        List<int[]> puzzleInputList = ParseInput.parseListWithTreeSpaces("/Users/yorkmacbook030/Desktop/Repos/Personal/advent-of-code/2024/src/main/java/com/advent2024/day01/day01-puzzleInput.txt");
+        
+
         // Add logic for Puzzle One
+        puzzleInputList.forEach(parts -> {
+            System.out.println("Part 1: " + parts[0] + ", Part 2: " + parts[1]);
+        });
+
+
     }
 
     @Override
@@ -16,5 +27,4 @@ public class JavaDayOne implements Day {
         // Add logic for Puzzle Two
     }
 
-    
 }
